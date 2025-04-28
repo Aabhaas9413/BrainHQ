4f0386554c16b5a4bede7f67aaddfd623ab0fc39
AABHAAS MALHOTRA <aabhaas9413@gmail.com>
2025-04-28 23:45:25 +0100

    Adding login and registration.

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginModel = {
+    email: '',
+    password: ''
+  };
 
+  registerModel = {
+    name: '',
+    email: '',
+    password: ''
+  };
+
+  onLoginSubmit() {
+    console.log('Login Data:', this.loginModel);
+    // Add logic to handle login
+  }
+
+  onRegisterSubmit() {
+    console.log('Registration Data:', this.registerModel);
+    // Add logic to handle registration
+  }
 }
